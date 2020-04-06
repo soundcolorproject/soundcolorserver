eea5069924bf8ef895b415b57478e3209dedd9a4
kgroat <kgroat09@gmail.com>
2020-04-05 23:04:47 -0400

    Getting rid of appColorsStore

--- site/state/MobxProvider.tsx
+++ site/state/MobxProvider.tsx
@@ -3,25 +3,22 @@ import * as React from 'react'
 import { Provider } from 'mobx-react'
 
 import { AnalysisProp, analysisStore } from './analysisStore'
-import { AppColorsProp, appColorsStore } from './appColorsStore'
 import { MediaProp, mediaStore } from './mediaStore'
 import { PatternsProp, patternsStore } from './patternsStore'
 import { RenderStateProp, renderStateStore } from './renderStateStore'
 
 interface Props {
-  children?: React.ReactNode,
+  children?: React.ReactNode
 }
 
 export type MobxProviderProps =
   & AnalysisProp
-  & AppColorsProp
   & MediaProp
   & PatternsProp
   & RenderStateProp
 
 const stores: MobxProviderProps = {
   analysis: analysisStore,
-  appColors: appColorsStore,
   media: mediaStore,
   patterns: patternsStore,
   renderState: renderStateStore,
