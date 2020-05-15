d0a7f88a3f2202d1027e63d0eb80361f40afc697
kgroat <kgroat09@gmail.com>
2020-05-14 21:20:58 -0400

    Also redirect to https

--- server/middleware/hsts.ts
+++ server/middleware/hsts.ts
@@ -1,8 +1,14 @@
 
 import { RequestHandler } from 'express'
+import { config } from '../config'
 
-const ONE_YEAR = 60 * 60 * 24 * 365
-export const hstsMiddleware: RequestHandler = (_req, res, next) => {
-  res.setHeader('Strict-Transport-Security', `max-age=${ONE_YEAR}; includeSubDomains`)
+const TWO_YEARS = 60 * 60 * 24 * 365 * 2
+export const hstsMiddleware: RequestHandler = (req, res, next) => {
+  res.setHeader('Strict-Transport-Security', `max-age=${TWO_YEARS}; includeSubDomains`)
+
+  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && !config.dev) {
+    return res.redirect('https://' + req.hostname + req.url)
+  }
+
   next()
 }
